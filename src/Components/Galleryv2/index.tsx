d9a5f8f1f9c3a32c44419d99130ef487b1670b5d
Faishal <faisal@tantowi.com>
2025-11-08 00:58:10 +0700

    fix: unify ourjoy footer

--- src/Components/Galleryv2/index.tsx
+++ src/Components/Galleryv2/index.tsx
@@ -3,10 +3,8 @@ import { AppContext } from "../../Utils/context";
 import { useContext } from "react";
 import css from "./Gallery.module.scss";
 import Flower from "../Flower";
-import joyella from "../../Images/joyella.svg";
-import instagram from "../../Images/joyellaInstagram.svg";
-import whatsapp from "../../Images/joyellaWhatsapp.svg";
 import CarouselGallery from "./CarouselGallery";
+import { Whatsapp } from "../Icons";
 
 const imagesAmount = Array.from(Array(19).keys());
 
@@ -17,6 +15,12 @@ let galleryImages = imagesAmount.map((_, i) => {
 
 const Gallery = () => {
   const { firstRenderHeight } = useContext(AppContext);
+
+  const whatsappNumber = "+6287815917683";
+  const message =
+    "Halo OurJoy by Joyella, apa bisa tanya tanya untuk pesan undangan online ?";
+  const whatsappUrl = new URL(`https://wa.me/${whatsappNumber}`);
+  whatsappUrl.searchParams.append("text", message);
 
   return (
     <div
@@ -43,16 +47,28 @@ const Gallery = () => {
         }}
       />
       <CarouselGallery images={galleryImages} />
-      <div className={css.joyellaContainer}>
-        <div className="font-family-bodoni-moda font-size-11">Made By</div>
-        <img src={joyella} alt="joyella" />
+      <div className={css.footer}>
+        <div className={css.logoContainer}>
+          <div>
+            <img
+              src="https://utfs.io/f/cda88ce7-1f16-41a8-a158-98f3c012e72c-5t0hc.svg"
+              alt="ourjoy"
+            />
+          </div>
+          <img
+            src="https://utfs.io/f/4cc98e52-3e08-42df-ae9f-c689b7f9ae8c-zb0pnc.svg"
+            alt="joyella"
+          />
+        </div>
+        <div className={css.divider}></div>
         <div>
           <a
-            href={`https://wa.me/6287815917683?text=%F0%9F%92%90%20Joyella%20E-Invitation%20%F0%9F%92%90%20Halo%20Joyella%2C%20apa%20bisa%20tanya%20tanya%20%26%20pesan%20e-invitation%3F%20%0A%0ABerikut%20data%20diri%20saya%0ANama%20%3D%0ANama%20akun%20ig%20%3D%0AKeperluan%20Acara%20%3D%20%28Wedding%2FEngagement%2FBirthday%2Flainnya%29%0A`}
+            href={whatsappUrl.toString()}
             target="_blank"
+            className={css.circleIcon}
             rel="noreferrer"
           >
-            <img src={whatsapp} alt="whatsapp" />
+            <Whatsapp size={20} />
           </a>
         </div>
       </div>
